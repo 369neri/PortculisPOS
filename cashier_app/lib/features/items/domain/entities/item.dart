7f8a150653e6a8930798f82cb3e5812db3057fa3
nyk <nyk@menabrealabs.com>
2023-03-12 23:24:14 +0700

    Renamed product item to trade item and added GTIN

--- cashier_app/lib/features/items/domain/entities/item.dart
+++ cashier_app/lib/features/items/domain/entities/item.dart
@@ -1,7 +1,7 @@
 import 'package:cashier_app/features/items/domain/entities/price.dart';
 
 abstract class Item {
-  String get sku;
-  String get label;
+  String? get sku;
+  String? get label;
   Price get unitPrice;
 }
--- cashier_app/lib/features/items/domain/entities/trade_item.dart
+++ cashier_app/lib/features/items/domain/entities/trade_item.dart
@@ -1,12 +1,16 @@
 import 'package:cashier_app/features/items/domain/entities/price.dart';
 import 'package:cashier_app/features/items/domain/entities/item.dart';
 
-class ProductItem implements Item {
+class TradeItem implements Item {
+  // Item fields overridden
   final String _sku;
   final String _label;
   final Price _unitPrice;
 
-  ProductItem(this._sku, this._label, this._unitPrice) : super();
+  // Fields specific to products (trade items)
+  final int? _gtin; // GTIN: Global Trade Item Number (barcodes)
+
+  TradeItem(this._sku, this._gtin, this._label, this._unitPrice) : super();
 
   @override
   String get sku => _sku;
@@ -17,4 +21,5 @@ class ProductItem implements Item {
   @override
   Price get unitPrice => _unitPrice;
 
+  int? get gtin => _gtin;
 }
