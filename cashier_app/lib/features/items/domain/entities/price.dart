5e7dcf7ce0717e676f98669b2888035eaeb6c49c
nyk <nyk@menabrealabs.com>
2023-03-12 20:34:44 +0700

    Fixed Price constructor and and negative test.

--- cashier_app/lib/features/items/domain/entities/price.dart
+++ cashier_app/lib/features/items/domain/entities/price.dart
@@ -1,18 +1,14 @@
 import 'package:get_it/get_it.dart';
 
 class Price {
-  final BigInt _price;
-  BigInt get price => _price;
+  final BigInt _value;
+  BigInt get value => _value;
 
-  Price(this._price) : super() {
-    throwIf(_price < BigInt.from(0), ArgumentError("prices cannot be negative"));
+  Price(this._value) : super() {
+    throwIf(_value.isNegative, ArgumentError("prices cannot be negative"));
   }
 
-  factory Price.fromInt(int price) {
-    return Price(BigInt.from(price));
-  }
-
-  factory Price.fromDouble(double price) {
+  factory Price.from(num price) {
     return Price(BigInt.from(price));
   }
 
