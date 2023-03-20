336afd720d87ea49cd2472271750d9d7f6a59f9c
nyk <nyk@menabrealabs.com>
2023-03-21 03:20:32 +0700

    Use fat arrow notation in PriceList class

--- cashier_app/lib/features/items/domain/entities/price_list.dart
+++ cashier_app/lib/features/items/domain/entities/price_list.dart
@@ -8,35 +8,16 @@ class PriceList {
 
   PriceList(this._currency) : super();
 
-  int countItems() {
-    return _items.length;
-  }
+  int countItems() => _items.length;
 
-  void addItem(Item item) {
-    _items.add(item);
-  }
+  void addItem(Item item) => _items.add(item);
+  void addItems(List<Item> items) => _items.addAll(items);
 
-  void addItems(List<Item> items) {
-    _items.addAll(items);
-  }
+  void insertItem(int index, Item item) => _items.insert(index, item);
+  void insertItems(int index, List<Item> items) => _items.insertAll(index, items);
 
-  void insertItem(int index, Item item) {
-    _items.insert(index, item);
-  }
+  void removeItem(Item item) => _items.remove(item);
+  void removeItemAt(int index) => _items.removeAt(index);
 
-  void insertItems(int index, List<Item> items) {
-    _items.insertAll(index, items);
-  }
-
-  void cancelItem(Item item) {
-    _items.remove(item);
-  }
-
-  void removeItemAt(int index) {
-    _items.removeAt(index);
-  }
-
-  void clear() {
-    _items.clear();
-  }
+  void clearAllItems() => _items.clear();
 }
